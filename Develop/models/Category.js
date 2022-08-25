21dc353dffd076a6eb20ce938bd3a94230637927
wjimh314 <wjimh314@gmail.com>
2022-08-25 17:43:18 -0400

    edits to category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,15 +6,25 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    //{
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    cantegory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category'
+    }
 );
 
 module.exports = Category;
