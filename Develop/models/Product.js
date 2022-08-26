d3263064724d69c3fa1fb7bc6bfeb2bed36a6638
wjimh314 <wjimh314@gmail.com>
2022-08-26 07:05:48 -0400

    working on product module

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,29 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // def{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  cantegory_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10,2),
+    allowNull: false,
+    validate: {
+      iseDecimal:true
+    }
+  },
+  
+
+  }
+
+
+
   },
   {
     sequelize,
