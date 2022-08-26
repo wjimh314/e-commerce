179b690d114b5204aef3f4d97b67088fbe6c1d3d
wjimh314 <wjimh314@gmail.com>
2022-08-26 09:31:09 -0400

    working on api routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,15 +5,61 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock','category_id'],
+    include: [
+    { model: Category,
+      attributes: ['id', 'category_name']
+    },
+    {
+    model : Tag,
+    attributes : ['id', 'tag_name']
+    }
+  ]
+})
+
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });   
+  
+
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+
+
+  router.get('/:id', (req, res) => {
+    Product.findOne({
+      where: {
+        id :req.params.id
+      };
+      attributes: ['id', 'product_name', 'price', 'stock','category_id'],
+      include: [
+      { model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+      model : Tag,
+      attributes : ['id', 'tag_name']
+      }
+    ]
+  })
+  
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json ({ message:'no product found'});
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 
 // create new product
 router.post('/', (req, res) => {
